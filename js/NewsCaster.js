61f9bc14c38f24190c3f6c7dc0b1c133be64edc4
kalishcode <zezinhosupsup@gmail.com>
2021-06-19 22:08:46 -0300

    ogimage

--- js/NewsCaster.js
+++ js/NewsCaster.js
@@ -6,10 +6,48 @@
 			if ('alb.reddit.com' not in news['href']) and ('/user/' not in news['href']):
 				await enviar_mensagens_unicas(canal_world_news, news.text+" <"+news['href']+">")
 */
+//https://blog.jim-nielsen.com/2020/a-cors-proxy-with-netlify/
 
-const PROXY = window.location.hostname == '0.0.0.0'?'https://cors-anywhere.herokuapp.com/':'/cors-proxy/';
+const PROXY = window.location.hostname == '0.0.0.0'?'':'/cors-proxy/';
 
-fetch(PROXY+'https://old.reddit.com/r/worldnews/top/?sort=top&t=day').then(response => response.text())
+function setBackgroundImg(URL) {	
+	document.body.style.cssText = `
+		background: url(${URL}) no-repeat center center fixed;
+		-webkit-background-size: cover;
+		-moz-background-size: cover;
+		-o-background-size: cover;
+		background-size: cover;
+	`;
+}
+
+fetch(PROXY+'https://old.reddit.com/r/worldnews/top/?sort=top&t=day')
+.then(response => {if(response.ok) return response.text()})
 .then(data => {
-    console.log(data);
+	const html = new DOMParser().parseFromString(data, 'text/html');
+	let news = html.querySelectorAll(`a[data-event-action="title"]`);
+	news = Array.from(news)
+	for(noticia of news.slice(0,1)) {
+		if(!noticia.href.includes('alb.reddit.com') && !noticia.href.includes('/user/')) {
+			console.log(noticia.href);
+			document.querySelector('p').innerHTML = noticia.innerHTML
+			fetch(PROXY+noticia.href)
+			.then(wtf => wtf.text())
+			.then(dados => {
+				const nova = new DOMParser().parseFromString(dados, 'text/html');
+				const ogimage = nova.querySelector('meta[property="og:image"]').content;
+				console.log(ogimage);
+				setBackgroundImg(ogimage)
+			});
+		}
+	}
 });
+
+/*
+async function asyncFn() {
+  const arr = ['a', 'b', 'c'];
+  for (const el of arr) {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    console.log(el);
+  }
+}
+*/
